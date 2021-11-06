ca862d987b23c408c53ef533ec010bb3711c2fcf
ryosei <ryo214_007@yahoo.co.jp>
2021-11-06 10:09:14 +0900

    課題提出Check2
    第2章チェックテスト

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,24 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+
+	private String firstName = "minami";
+	private String lastName = "ryosei";
+	
+	public static void main(String[] args) {
+	
+		System.out.println("printNameメソッド →" + printName(" 七海","真弥"));
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+		
+	}
+	
+    private static String printName(String firstname, String lastName) {
+		    return firstname + lastName;
+	}
+}
