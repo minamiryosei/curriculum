eb7807f4523890e29ac1e920c6b64d3bae916125
ryosei <ryo214_007@yahoo.co.jp>
2022-02-07 18:37:59 +0900

    課題提出Check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -4,21 +4,21 @@ import constants.Constants;
 
 public class Check {
 
-	private String firstName = "minami";
-	private String lastName = "ryosei";
-	
-	public static void main(String[] args) {
-	
-		System.out.println("printNameメソッド →" + printName(firstName, lastName));
-		
-		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
-		pet.introduce();
-		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
-		robotPet.introduce();
-		
-	}
-	
-    private static String printName(String firstname, String lastName) {
-		    return firstname + lastName;
-	}
+private static String firstName = "minami";
+private static String lastName = "ryosei";
+
+public static void main(String[] args) {
+
+	System.out.println("printNameメソッド →" + printName(firstName, lastName));
+
+	Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+	pet.introduce();
+	RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+	robotPet.introduce();
+
+}
+
+private static String printName(String firstname, String lastName) {
+	    return firstname + lastName;
+}
 }
